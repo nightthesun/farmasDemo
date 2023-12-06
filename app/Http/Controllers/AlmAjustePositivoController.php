263163ce0c73e07d9a76dd571a0ffd8925c16ac0
nightthesun <remberto-cori-alejo@outlook.com>
2023-12-06 18:28:43 -0400

    feat:pasitivos y negativos

---
+++ app/Http/Controllers/AlmAjustePositivoController.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Alm_AjustePositivo;
+use Illuminate\Http\Request;
+
+class AlmAjustePositivoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    public function listarProductoLineaIngreso(){
+
+    }
+    
+    /**
+     * Store a newly created resource in storage.
+     */
+
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Alm_AjustePositivo $alm_AjustePositivo)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Alm_AjustePositivo $alm_AjustePositivo)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Alm_AjustePositivo $alm_AjustePositivo)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Alm_AjustePositivo $alm_AjustePositivo)
+    {
+        //
+    }
+}

bcec8a57b60238874a1219549b881456a3da7bfc
nightthesun <remberto-cori-alejo@outlook.com>
2023-12-15 20:51:47 -0400

    f

--- resources/js/components/inventario/AjusteNegativoComponent.vue
+++ resources/js/components/inventario/AjusteNegativoComponent.vue
@@ -2,7 +2,7 @@
     <main class="main">
         <!-- Breadcrumb -->
         <ol class="breadcrumb">
-            <li class="breadcrumb-item">Home</li>
+            <li class="breadcrumb-item">Home111</li>
             <li class="breadcrumb-item"><a href="#">Admin</a></li>
             <li class="breadcrumb-item active">Dashboard</li>
       
